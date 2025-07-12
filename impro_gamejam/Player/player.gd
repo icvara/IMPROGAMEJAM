2cf5efc30b6c2739a462015212c7a5032b258be9
Icvara <icvara.aor@gmail.com>
2025-07-12 21:09:55 +0100

    added animation ugly

--- impro_gamejam/Player/player.gd
+++ impro_gamejam/Player/player.gd
@@ -29,25 +29,59 @@ func _input(event):
 		
 func _physics_process(delta: float) -> void:
 	if alive:
-		input_direction = Input.get_axis("left", "right")
-		if Input.is_action_pressed("left"):
-			$AnimatedSprite2D.flip_h = true
-			last_dir = -1
-		elif Input.is_action_pressed("right"):
-			$AnimatedSprite2D.flip_h = false
-			last_dir = 1
-		velocity.x = input_direction * speed
+
+
+		
+
 		if not is_on_floor() :
 			velocity.y += GRAVITY * delta
+			
+			if part.has("plume"):
+				$plume_animation.hide()
+				$AnimatedSprite2D.play("jump_plume")
+			else:
+				$AnimatedSprite2D.play("jump")
 		else:
+			if n_jump >0:
+				$AnimatedSprite2D.play("default")
 			n_jump = 0
+			if part.has("plume"):
+					$plume_animation.show()
+			
+
 		
 		if Input.is_action_just_pressed("jump") and n_jump < max_jump:
 			$Sound/Bloop.play()
+			if part.has("plume"):
+				$plume_animation.hide()
+				if n_jump == 1:
+					$AnimatedSprite2D.play("jump_plume2")
+				else:
+					$AnimatedSprite2D.play("jump_plume")
+			else:
+				$AnimatedSprite2D.play("jump")
 			velocity.y = -jump_speed
 			n_jump += 1
 		
+		input_direction = Input.get_axis("left", "right")
+		if Input.is_action_pressed("left"):
+			if is_on_floor() :
+				$AnimatedSprite2D.flip_h = true
+				$oreille_animation.flip_h = true
+				$plume_animation.flip_h = true
+				$AnimatedSprite2D.play("walk")
 		
+			last_dir = -1
+		elif Input.is_action_pressed("right"):
+			if is_on_floor() :
+				$AnimatedSprite2D.flip_h = false
+				$oreille_animation.flip_h = false
+				$plume_animation.flip_h = false
+				$AnimatedSprite2D.play("walk")
+			last_dir = 1
+
+	velocity.x = input_direction * speed
+
 	move_and_slide() 
 
 
@@ -63,10 +97,13 @@ func get_hurt():
 				if select_part == "oreille":
 					oreille_platform = -1
 					lost_part = oreille.instantiate()
+					$oreille_animation.hide()
 					
 				elif select_part == "plume":
 					max_jump = 1
 					lost_part = plume.instantiate()
+					$plume_animation.hide()
+
 
 				$AnimatedSprite2D.play(part[0])
 			else:
@@ -75,10 +112,13 @@ func get_hurt():
 					oreille_platform = -1
 					lost_part = oreille.instantiate()
 					$AnimatedSprite2D.play("default")
+					$oreille_animation.hide()
+
 				elif select_part == "plume":
 					max_jump = 1
 					$AnimatedSprite2D.play("default")
 					lost_part = plume.instantiate()
+					$plume_animation.hide()
 			get_parent().add_child(lost_part)
 			lost_part.global_position = position -Vector2(0,100)
 			lost_part.apply_force(Vector2(-last_dir*20000,-1*2000),position)
@@ -100,8 +140,8 @@ func get_hurt():
 
 func action():
 	if part.has("oreille"):
-		print(oreille_platform)
 		if oreille_platform == 1 :
+			$oreille_animation.play("action")
 			oreille_platform = 0
 			var plt = platform.instantiate()
 			get_parent().add_child(plt)
@@ -109,18 +149,22 @@ func action():
 			await get_tree().create_timer(3.0).timeout
 			print("timer waited")
 			oreille_platform =1
-				
+			$oreille_animation.play("default")
+
 	
 	pass
 
 func power_up(blop):
 	if blop.object_id == "plume":
-		$AnimatedSprite2D.play("plume")
+		#$AnimatedSprite2D.play("plume")
+		$plume_animation.show()
 		if part.has("plume") == false:
 			part.append("plume")
 			max_jump = 2
 	elif blop.object_id == "oreille":
-		$AnimatedSprite2D.play("oreille")
+		#$AnimatedSprite2D.play("oreille")
+		$oreille_animation.show()
+
 		if part.has("oreille") == false:
 			part.append("oreille")
 			oreille_platform = 1
