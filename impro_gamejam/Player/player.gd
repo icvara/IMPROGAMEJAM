46be79171e01625b434e76efdae0231a3b102aa1
Icvara <icvara.aor@gmail.com>
2025-07-12 17:12:28 +0100

    death is here

--- impro_gamejam/Player/player.gd
+++ impro_gamejam/Player/player.gd
@@ -6,20 +6,24 @@ extends CharacterBody2D
 @export var jump_speed = 500
 @export var GRAVITY = 9.8 * 100
 
+var alive = true
 
 func _input(event):
 	if event.is_action_pressed("restart"):
+		alive =true
 		get_tree().reload_current_scene()
 		
 func _physics_process(delta: float) -> void:
-	var input_direction = Input.get_axis("left", "right")
-	velocity.x = input_direction * speed
-	if not is_on_floor():
-		velocity.y += GRAVITY * delta
-	
-	if Input.is_action_just_pressed("jump"):
-		print("jump")
-		velocity.y = -jump_speed
-	
-	
+	if alive:
+		var input_direction = Input.get_axis("left", "right")
+		velocity.x = input_direction * speed
+		if not is_on_floor():
+			velocity.y += GRAVITY * delta
+		
+		if Input.is_action_just_pressed("jump"):
+			print("jump")
+			$Sound/Bloop.play()
+			velocity.y = -jump_speed
+		
+		
 	move_and_slide()
