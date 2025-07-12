39b8ff6e41628c16c1dee2624d01f67d000221f2
Icvara <icvara.aor@gmail.com>
2025-07-12 21:18:31 +0100

    fddsf

--- impro_gamejam/win_flag.gd
+++ impro_gamejam/win_flag.gd
@@ -2,6 +2,9 @@ extends Node2D
 @onready var mainmenu = preload("res://Menus.tscn")
 
 
+func _enter_tree() -> void:
+	mainmenu = preload("res://Menus.tscn")
+
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	print("test2d")
 	$Label.show()
@@ -10,4 +13,4 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 
 
 func _on_return_to_menu_button_down() -> void:
-	get_tree().change_scene_to_packed(mainmenu)
+	get_tree().change_scene_to_file("res://Menus.tscn")
